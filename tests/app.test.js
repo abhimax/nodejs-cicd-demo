b7c7f8c5f9717e11d20b52055756ec3c25549066
Abhiman Ranaweera <abhiman.max@gmail.com>
2025-08-10 23:39:56 +0530

    test: add app unit test

---
+++ tests/app.test.js
@@ -0,0 +1,11 @@
+// tests/app.test.js
+const request = require("supertest");
+const app = require("../app");
+
+describe("GET /", () => {
+  it("should return Hello, GitHub Actions!", async () => {
+    const response = await request(app).get("/");
+    expect(response.statusCode).toBe(200);
+    expect(response.text).toBe("Hello, GitHub Actions!");
+  });
+});
